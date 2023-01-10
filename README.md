1c2bda987005c272ee72ff87c84d36b79a00413a
carlosnunesjr <carlosnunes@gmail.com>
2023-01-10 19:27:52 -0300

    add memo, useMemo and useCallback

---
+++ README.md
@@ -0,0 +1,42 @@
+# Otimização ( WatchMe )
+
+## 📚 Informações sobre o projeto
+
+- Esse projeto foi desenvolvido como parte do curso de reactjs da Rocketseat
+
+&nbsp;
+
+## 💻 Quais eram os requisitos mínimos do desafio?
+
+- Aplicar o uso do memo, useMemo e useCallback.
+
+&nbsp;
+
+## 🛠️ Tecnologias/Ferramentas ultilizadas
+
+- [React](https://pt-br.reactjs.org/)
+- [json-server](https://github.com/typicode/json-server)
+
+&nbsp;
+
+## ⚙️ Instalação
+
+```
+# Abra um terminal e clone este repositório com o comando
+$ git clone https://github.com/carlosnunesjr/react-otimization.git
+```
+
+```
+# Acesse a pasta da aplicação
+$ cd react-otimization
+
+# Instale as dependências
+$ yarn
+
+# Rode o servidor fake de json em um terminal a parte
+$ yarn server
+
+# Inicie a aplicação em modo de desenvolvimento
+$ yarn dev
+
+```
--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,9 +1,10 @@
+import { useMemo } from "react";
 import { MovieCard } from "./MovieCard";
 
 interface ContentProps {
   selectedGenre: {
     id: number;
-    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    name: "action" | "comedy" | "documentary" | "drama" | "horror" | "family";
     title: string;
   };
 
@@ -20,19 +21,36 @@ interface ContentProps {
 }
 
 export function Content({ selectedGenre, movies }: ContentProps) {
+  const ratingMedio = useMemo(() => {
+    const totalRating = movies.reduce((total, movie) => {
+      return total + Number(movie.Ratings[0].Value.split("/")[0]);
+    }, 0);
+
+    return totalRating / movies.length;
+  }, [movies]);
+
   return (
     <div className="container">
       <header>
-        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        <span className="category">
+          Categoria:<span> {selectedGenre.title}</span> ( {movies.length} )
+          Rating médio: {ratingMedio.toFixed(2)}
+        </span>
       </header>
 
       <main>
         <div className="movies-list">
           {movies.map(movie => (
-            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            <MovieCard
+              key={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
           ))}
         </div>
       </main>
     </div>
-  )
+  );
 }
